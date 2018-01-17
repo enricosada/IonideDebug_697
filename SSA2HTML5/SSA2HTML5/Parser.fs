a1810417105ed645ddfa87ef763a38377b0652cb
Enrico Sada <enrico@sada.io>
2018-01-17 12:18:16 +0100

    initial commit

---
+++ SSA2HTML5/SSA2HTML5/Parser.fs
@@ -0,0 +1,8 @@
+module Parser
+open FParsec
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
